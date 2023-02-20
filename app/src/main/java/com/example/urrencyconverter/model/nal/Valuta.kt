89a6ff91caf9bec24a38808c48f5bc9c7630ed90
Admin <urmasuper93@gmail.com>
2023-02-21 02:04:11 +0300

    make adaptor

---
+++ app/src/main/java/com/example/urrencyconverter/model/nal/Valuta.kt
@@ -0,0 +1,11 @@
+package com.example.urrencyconverter.model.nal
+
+data class Valuta(
+    val id: String,
+    val NumCode: String,
+    val CharCode: String,
+    val Nominal: String,
+    val Name: String,
+    val Value: String,
+    val Previous: String
+)
--- app/src/main/java/com/example/urrencyconverter/screens/start/StartAdapter.kt
+++ app/src/main/java/com/example/urrencyconverter/screens/start/StartAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.urrencyconverter.screens.start
 
-class StartAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.urrencyconverter.R
+import com.example.urrencyconverter.model.nal.Valuta
+import com.example.urrencyconverter.model.nal.ValuteItem
+import kotlinx.android.synthetic.main.item_money_layout.view.*
+
+class StartAdapter:RecyclerView.Adapter<StartAdapter.StartViewHolder>() {
+
+     var listStart = emptyList<Valuta>()
+
+    class StartViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StartViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_money_layout,parent,false)
+        return StartViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StartViewHolder, position: Int) {
+        holder.itemView.item_name.text = listStart[position].Name
+        holder.itemView.item_buy.text = listStart[position].Value
+        holder.itemView.item_sale.text = listStart[position].Previous
+    }
+
+    override fun getItemCount(): Int {
+       return listStart.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(list: List<Valuta>)
+    {
+        listStart = list
+        notifyDataSetChanged()
+    }
+
 }
